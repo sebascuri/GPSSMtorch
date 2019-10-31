6778078f2fdd59d80fb61e4b1bb998948cf96fce
scuri <sebastian.curi@inf.ethz.ch>
2019-10-31 17:56:46 +0100

    Add learnable covariance matrix to recognition model

--- gpssm/models/components/recognition_model.py
+++ gpssm/models/components/recognition_model.py
@@ -15,12 +15,18 @@ class Recognition(nn.Module):
         self.prior = MultivariateNormal(torch.zeros(dim_states),
                                         covariance_matrix=torch.eye(dim_states))
 
+    def copy(self):
+        """Copy recognition model."""
+        raise NotImplementedError
+
 
 class OutputRecognition(Recognition):
     """Recognition model based on the outputs of the first time-step."""
 
-    def __init__(self, dim_states: int) -> None:
+    def __init__(self, dim_states: int, sd_noise: float = 1.0) -> None:
         super().__init__(dim_states)
+        self.sd_noise = nn.Parameter(torch.ones(self.dim_states) * sd_noise,
+                                     requires_grad=True)
 
     def forward(self, output_sequence: Tensor,
                 input_sequence: Tensor) -> MultivariateNormal:
@@ -29,4 +35,8 @@ class OutputRecognition(Recognition):
         loc = torch.zeros(self.dim_states)
         loc[:dim_outputs] = output_sequence[0]
 
-        return self.posterior
+        return MultivariateNormal(loc, covariance_matrix=torch.diag(self.sd_noise ** 2))
+
+    def copy(self) -> Recognition:
+        """Copy recognition model."""
+        return OutputRecognition(self.dim_states)
