f85a084e9fbf637be1c602dd5e3515e44db6dbcb
scuri <sebastian.curi@inf.ethz.ch>
2019-11-06 13:33:58 +0100

    Change ways of getting inducing points

--- gpssm/models/utilities.py
+++ gpssm/models/utilities.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def get_inducing_points(num_inducing_points: int, dim_inputs: int,
-                        strategy: str = 'random') -> torch.Tensor:
+                        strategy: str = 'normal', scale: float = 1) -> torch.Tensor:
     """Initialize inducing points for variational GP.
 
     Parameters
@@ -15,7 +15,10 @@ def get_inducing_points(num_inducing_points: int, dim_inputs: int,
     dim_inputs: int.
         Input dimensionality.
     strategy: str, optional.
-        Strategy to generate inducing points (either random, by default, or uniform).
+        Strategy to generate inducing points (by default normal).
+        Either either normal, uniform or linspace.
+    scale: float, optional.
+        Scale of inducing points (default 1)
 
     Returns
     -------
@@ -23,10 +26,13 @@ def get_inducing_points(num_inducing_points: int, dim_inputs: int,
         Inducing points with shape [dim_outputs x num_inducing_points x dim_inputs]
 
     """
-    if strategy == 'random':
-        ip = torch.randn((num_inducing_points, dim_inputs))
+    if strategy == 'normal':
+        ip = scale * torch.randn((num_inducing_points, dim_inputs))
     elif strategy == 'uniform':
-        ip = np.linspace(-1, 1, np.ceil(num_inducing_points ** (1 / dim_inputs)))
+        ip = scale * torch.rand((num_inducing_points, dim_inputs)) - (scale / 2)
+    elif strategy == 'linspace':
+        lin_points = int(np.ceil(num_inducing_points ** (1 / dim_inputs)))
+        ip = np.linspace(-scale, scale, lin_points)
         ip = np.array(np.meshgrid(*([ip] * dim_inputs))).reshape(dim_inputs, -1).T
         idx = np.random.choice(np.arange(ip.shape[0]), size=num_inducing_points,
                                replace=False)
