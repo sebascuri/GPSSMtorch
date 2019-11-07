b928af50679011200c417e48513309210ac27169
scuri <sebastian.curi@inf.ethz.ch>
2019-11-07 11:00:31 +0100

    Add testing coverage

--- gpssm/models/components/emissions.py
+++ gpssm/models/components/emissions.py
@@ -1,10 +1,10 @@
 """Emission model for GPSSM's."""
 from gpytorch.likelihoods import Likelihood
 from gpytorch.distributions import MultivariateNormal
-from gpytorch.likelihoods.noise_models import Noise
 from torch import Tensor
 from torch.nn import ModuleList
-from typing import Union, Any, List
+from typing import Union, List
+State = Union[Tensor, MultivariateNormal]
 
 __author__ = 'Sebastian Curi'
 __all__ = ['Emissions']
@@ -27,14 +27,29 @@ class Emissions(Likelihood):
     >>> from torch.testing import assert_allclose
     >>> dim_states, dim_outputs = 3, 2
     >>> num_particles = 8
-    >>> emission = Emissions([GaussianLikelihood(), GaussianLikelihood()])
+    >>> emissions = Emissions([GaussianLikelihood(), GaussianLikelihood()])
+    >>> debug_str = str(emissions)
     >>> d = MultivariateNormal(torch.zeros(dim_states), torch.eye(dim_states))
     >>> x = d.rsample(sample_shape=torch.Size([num_particles]))
     >>> c = torch.zeros((dim_states, dim_outputs))
     >>> c[:dim_outputs, :dim_outputs] = torch.eye(dim_outputs)
-    >>> assert_allclose(emission(x)[0].loc, (x @ c)[:, 0])
-    >>> assert_allclose(emission(x)[0].loc.shape, Size([num_particles]))
-    >>> assert_allclose(emission(x)[0].scale.shape, Size([num_particles]))
+    >>> for i in range(dim_outputs):
+    ...     assert_allclose(emissions(x)[i].loc, (x @ c)[:, i])
+    ...     assert_allclose(emissions(x)[i].loc.shape, Size([num_particles]))
+    ...     assert_allclose(emissions(x)[i].scale.shape, Size([num_particles]))
+    >>> loc = torch.randn(num_particles, dim_states)
+    >>> cov = 0.1 * torch.eye(dim_states).expand(num_particles, dim_states, dim_states)
+    >>> d = MultivariateNormal(loc, cov)
+    >>> y = emissions(d)
+    >>> for i in range(dim_outputs):
+    ...     assert_allclose(emissions.forward(d)[i].loc[:, 0], loc[:, i])
+    ...     assert_allclose(emissions.forward(d)[i].loc.shape, Size([num_particles, 1]))
+    ...     assert_allclose(emissions.forward(d)[i].covariance_matrix.shape,
+    ...         Size([num_particles, 1, 1]))
+    >>> x = [torch.randn(32, num_particles) for _ in range(dim_states)]
+    >>> for i in range(dim_outputs):
+    ...     assert_allclose(emissions(x)[i].loc.shape, Size([32, num_particles]))
+    ...     assert_allclose(emissions(x)[i].scale.shape, Size([32, num_particles]))
     """
 
     def __init__(self, likelihoods: List[Likelihood]) -> None:
@@ -50,61 +65,42 @@ class Emissions(Likelihood):
                 i, str(self.likelihoods[i].noise_covar.noise.detach()))
         return string
 
-    def __call__(self, state: Union[Tensor, MultivariateNormal,
-                                    List[MultivariateNormal]],
-                 *args, **kwargs) -> List[MultivariateNormal]:
-        """Call the emission model for a given state.
+    def __call__(self, state: Union[State, List[State]], *args, **kwargs
+                 ) -> List[MultivariateNormal]:
+        """See `self.forward'."""
+        return self.forward(state, *args, **kwargs)
+
+    def forward(self, state: Union[State, List[State]], *args, **kwargs
+                ) -> List[MultivariateNormal]:
+        """Compute the conditional or marginal distribution of the emissions.
+
+         If f_samples is a Tensor (or a List of Tensors) then compute the conditional
+         p(y|f).
+         If f_samples is a MultivariateNormal (or a List of Multivariate Normals) then
+         compute the marginal p(y).
 
         Parameters
         ----------
-        state: Tensor, MultivariateNormal or list of MultivariateNormal.
-            State tensor of size [batch_size x num_particles x dim_state] or,
-            MultivariateNormal of size [batch_size x dim_state] or,
-            list w/ length dim_state of MultivariateNormal [batch_size x num_particles].
+        state: State or List of State.
+            State of dimension dim_state x batch_size x num_particles or a list of
+            length dim_state of State with dimension batch_size x num_particles.
 
         Returns
         -------
-        output_distribution: List of MultivariateNormal
-            list w/ len dim_output of MultivariateNormal [batch_size x num_particles].
-
+        y: list of MultivariateNormal.
+            List of length dim_output of MultivariateNormal with dimension batch_size x
+            num_particles.
         """
         if type(state) is Tensor:
             return [self.likelihoods[i](state[..., i]) for i in range(self.dim_outputs)]
 
         elif type(state) is MultivariateNormal:
             return [self.likelihoods[i](MultivariateNormal(
-                state.loc[:, i:(i+1)],
+                state.loc[:, i:(i + 1)],
                 state.covariance_matrix[:, i:(i + 1), i:(i + 1)]))
                 for i in range(self.dim_outputs)]
         elif type(state) is list:
             return [self.likelihoods[i](state[i]) for i in range(self.dim_outputs)]
         else:
-            raise TypeError('Type {} of state not understood'.format(type(state)))
-
-    def forward(self, state: List[Tensor], *args, **kwargs
-                ) -> List[MultivariateNormal]:
-        """Compute the conditional distribution p(y|f) that defines the likelihoods.
-
-        Parameters
-        ----------
-        state: list of Tensor.
-            Samples from function `f`.
-
-        Returns
-        -------
-        y: list of MultivariateNormal.
-            Distribution object (with same shape as `function_samples`).
-        """
-        return [self.likelihoods[i](state[i]) for i in range(self.dim_outputs)]
-
-    def noise_covar(self) -> Noise:
-        """Return component noise covariance."""
-        return self.likelihoods.noise_covar
-
-    def expected_log_prob(self, measurement: Tensor,
-                          state: List[MultivariateNormal],
-                          *params: Any, **kwargs: Any) -> List[Tensor]:
-        """Return the expected log prob of a `tensor' under the `state' distribution."""
-        return [self.likelihoods[i].expected_log_prob(measurement[..., i], state[i],
-                                                      *params, **kwargs)
-                for i in range(self.dim_outputs)]
+            raise NotImplementedError('Type {} of state not implemented'.format(
+                type(state)))
--- gpssm/models/components/recognition_model.py
+++ gpssm/models/components/recognition_model.py
@@ -3,7 +3,11 @@
 from torch import Tensor
 import torch
 import torch.nn as nn
+import numpy as np
 from gpytorch.distributions import MultivariateNormal
+
+__author__ = 'Sebastian Curi'
+__all__ = ['Recognition', 'OutputRecognition']
 
 
 class Recognition(nn.Module):
@@ -25,11 +29,36 @@ class Recognition(nn.Module):
 
 
 class OutputRecognition(Recognition):
-    """Recognition model based on the outputs of the first time-step."""
+    """Recognition model based that uses the outputs of the first time step.
 
-    def __init__(self, dim_states: int, sd_noise: float = 1.0) -> None:
+    Parameters
+    ----------
+    dim_states: int.
+        Dimension of the state.
+
+    variance: float, optional.
+        Initial variance of the noise.
+
+    Examples
+    --------
+    >>> from torch.testing import assert_allclose
+    >>> recognition = OutputRecognition(4, variance=0.01)
+    >>> assert_allclose(recognition.sd_noise, torch.ones(4) * 0.1)
+    >>> output_seq = torch.randn(32, 8, 2)
+    >>> input_seq = torch.randn(32, 8, 1)
+    >>> x0 = recognition(output_seq, input_seq)
+    >>> assert_allclose(x0.loc[:, :2], output_seq[:, 0])
+    >>> cov = torch.diag(torch.ones(4) * 0.01).expand(32, 4, 4)
+    >>> assert_allclose(x0.covariance_matrix, cov)
+    >>> debug_str = str(recognition)
+    >>> other = recognition.copy()
+    >>> assert other is not recognition
+    >>> assert type(other) == type(recognition)
+    """
+
+    def __init__(self, dim_states: int, variance: float = 1.0) -> None:
         super().__init__(dim_states)
-        self.sd_noise = nn.Parameter(torch.ones(self.dim_states) * sd_noise,
+        self.sd_noise = nn.Parameter(torch.ones(self.dim_states) * np.sqrt(variance),
                                      requires_grad=True)
 
     def forward(self, output_sequence: Tensor,
