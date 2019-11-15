087bd23d1cab2bc0272341ed9a490215ca3ad12f
scuri <sebastian.curi@inf.ethz.ch>
2019-11-15 17:38:13 +0100

    Add conditioning to GPSSM

--- gpssm/models/ssm_vi.py
+++ gpssm/models/ssm_vi.py
@@ -1,38 +1,59 @@
 """Base Class for System Id using Variational Inference with SSMs."""
 
-from torch import Tensor
+from torch import Tensor, Size
 import torch
 import torch.jit
 import torch.nn as nn
 from torch.distributions import kl_divergence, Normal
 from typing import List
+from gpytorch.distributions import MultivariateNormal
 
 from .components.gp import ModelList
 from .components.emissions import Emissions
 from .components.transitions import Transitions
 from .components.recognition_model import Recognition
 
-
 __author__ = 'Sebastian Curi'
-__all__ = ['SSMSVI']
+__all__ = ['PRSSM', 'CBFSSM']
 
 
-class SSMSVI(nn.Module):
-    """Abstract Base Class for Stochastic Variational Inference algorithms on SSMs."""
+class PRSSM(nn.Module):
+    """Implementation of PR-SSM Algorithm."""
+
     def __init__(self,
                  forward_model: ModelList,
                  transitions: Transitions,
                  emissions: Emissions,
                  recognition_model: Recognition,
+                 num_particles: int,
+                 backward_model: ModelList = None,
+                 cubic_sampling: bool = False
                  ) -> None:
         super().__init__()
         self.dim_states = forward_model.num_outputs
         self.forward_model = forward_model
+        self.backward_model = backward_model
         self.transitions = transitions
         self.emissions = emissions
 
         self.prior_recognition = recognition_model.copy()
         self.posterior_recognition = recognition_model.copy()
+
+        self.num_particles = num_particles
+        self.cubic_sampling = cubic_sampling
+
+    def __str__(self) -> str:
+        """Return string of object with parameters."""
+        string = "Model Parameters: \n\n"
+        string += "Forward Model {}\n".format(self.forward_model)
+        if self.backward_model is not None:
+            string = "Backward Model {}\n".format(self.backward_model)
+        string += "Emission {}\n".format(self.emissions)
+        string += "Transition {}\n".format(self.transitions)
+        string += "Prior x1 {}\n".format(self.prior_recognition)
+        string += "Posterior x1 {}\n".format(self.posterior_recognition)
+
+        return string
 
     def properties(self) -> list:
         """Return list of learnable parameters."""
@@ -71,9 +92,9 @@ class SSMSVI(nn.Module):
         l2 = torch.tensor(0.)
         for t in range(sequence_length):
             # Output: Torch (dim_outputs)
-            y = output_sequence[:, t]  # .expand(num_particles, batch_size, dim_outputs)
-            # y = y.permute(1, 0, 2)
-            assert y.shape == torch.Size([batch_size, dim_outputs])
+            y = output_sequence[:, t].expand(1, batch_size, dim_outputs)
+            y = y.permute(2, 1, 0)
+            assert y.shape == torch.Size([dim_outputs, batch_size, 1])
 
             y_pred = predicted_outputs[t]
             ############################################################################
@@ -88,8 +109,8 @@ class SSMSVI(nn.Module):
         ################################################################################
         kl_u = self.forward_model.kl_divergence()  # type: ignore
         kl_x1 = kl_divergence(
-            self.posterior_recognition(output_sequence, input_sequence),  # type: ignore
-            self.prior_recognition(output_sequence, input_sequence)  # type: ignore
+            self.posterior_recognition(output_sequence, input_sequence),
+            self.prior_recognition(output_sequence, input_sequence)
         ).mean()
 
         ################################################################################
@@ -105,12 +126,11 @@ class SSMSVI(nn.Module):
             return l2
         elif key.lower() == 'rmse':
             return torch.sqrt(l2)
-        elif key.lower() == 'elbo_separated':
-            return log_lik, kl_x1, kl_u  # type: ignore
         else:
             raise NotImplementedError("Key {} not implemented".format(key))
 
-    def forward(self, *inputs: Tensor) -> List[Normal]:  # type: ignore
+    @torch.jit.export
+    def forward(self, *inputs: Tensor, **kwargs) -> List[Normal]:
         """Forward propagate the model.
 
         Parameters
@@ -124,7 +144,182 @@ class SSMSVI(nn.Module):
 
         Returns
         -------
-        output_distribution: Normal.
-            Normal of prediction_length x dim_outputs
+        output_distribution: List[List[MultivariateNormal].
+            List of list of distributions [prediction_length x dim_outputs x
+            batch_size x num_particles].
         """
-        raise NotImplementedError
+        output_sequence, input_sequence = inputs
+        num_particles = self.num_particles
+        dim_states = self.dim_states
+        batch_size, sequence_length, dim_inputs = input_sequence.shape
+
+        # Initial State: Tensor (batch_size x num_particles x dim_states)
+        if self.training:
+            state_d = self.posterior_recognition(output_sequence, input_sequence)
+        else:
+            print('evaluating')
+            state_d = self.prior_recognition(output_sequence, input_sequence)
+
+        state = state_d.rsample(sample_shape=torch.Size([num_particles]))
+        state = state.permute(1, 0, 2)
+        assert state.shape == Size([batch_size, num_particles, dim_states])
+
+        ############################################################################
+        # SAMPLE GP for cubic sampling #
+        ############################################################################
+        # if self.cubic_sampling:
+        #     # TODO: Change inducing points only (and inducing variables) :).
+        #     forward_model = self.forward_model.sample_gp(
+        #         self.transitions.likelihoods)  # type: ignore
+        #     forward_model.eval()
+        # else:
+        #     forward_model = self.forward_model
+
+        ############################################################################
+        # PERFORM Backward Pass #
+        ############################################################################
+        if self.training:
+            output_distribution = self._backward(output_sequence)
+
+        ############################################################################
+        # PREDICT Outputs #
+        ############################################################################
+        outputs = []
+        y_pred = self.emissions(state)
+        outputs.append(y_pred)
+
+        for t in range(sequence_length - 1):
+            ############################################################################
+            # PREDICT Next State #
+            ############################################################################
+
+            # Input: Torch (batch_size x num_particles x dim_inputs)
+            u = input_sequence[:, t].expand(num_particles, batch_size, dim_inputs)
+            u = u.permute(1, 0, 2)
+            assert u.shape == Size([batch_size, num_particles, dim_inputs])
+
+            # \hat{X}: Torch (batch_size x num_particles x dim_states + dim_inputs)
+            state_input = torch.cat((state, u), dim=-1)
+            assert state_input.shape == Size(
+                [batch_size, num_particles, dim_inputs + dim_states])
+
+            # next_f: Multivariate Normal (state_dim x batch_size x num_particles)
+            next_f = self.forward_model(state_input)
+            assert next_f.loc.shape == Size([dim_states, batch_size, num_particles])
+            assert next_f.covariance_matrix.shape == Size(
+                [dim_states, batch_size, num_particles, num_particles])
+
+            # next_state: Multivariate Normal (state_dim x batch_size x num_particles)
+            next_state = self.transitions(next_f)
+            assert next_f.loc.shape == Size([dim_states, batch_size, num_particles])
+            assert next_f.covariance_matrix.shape == Size(
+                [dim_states, batch_size, num_particles, num_particles])
+            assert (next_state.loc == next_f.loc).all()
+            assert not (next_state.covariance_matrix == next_f.covariance_matrix).all()
+
+            ############################################################################
+            # UPDATE GP for cubic sampling #
+            ############################################################################
+            # if self.cubic_sampling:
+            #     forward_model = forward_model.get_fantasy_model(state_input, next_f)
+            # assert forward_model.num_outputs == dim_states
+
+            ############################################################################
+            # CONDITION Next State #
+            ############################################################################
+            if self.training:
+                next_state = self._condition(next_state, output_distribution[t + 1])
+
+            ############################################################################
+            # RESAMPLE State #
+            ############################################################################
+
+            # state: Tensor (batch_size x num_particles x dim_states)
+            state_d = next_state
+            state = state_d.rsample().permute(1, 2, 0)
+            assert state.shape == Size([batch_size, num_particles, dim_states])
+
+            ############################################################################
+            # PREDICT Outputs #
+            ############################################################################
+
+            y_pred = self.emissions(state)
+            outputs.append(y_pred)
+
+        assert len(outputs) == sequence_length
+        return outputs
+
+    def _condition(self, next_x: MultivariateNormal, next_y: Normal
+                   ) -> MultivariateNormal:
+        """Implement conditioning."""
+        return next_x
+
+    def _backward(self, output_sequence: Tensor) -> List[Normal]:
+        """Implements backwards pass."""
+        batch_size, sequence_length, dim_outputs = output_sequence.shape
+        dim_states = self.dim_states
+        num_particles = self.num_particles
+        dim_delta = dim_states - dim_outputs
+
+        outputs = []
+        for t in reversed(range(sequence_length)):
+            y = output_sequence[:, t]
+            y_ = self.emissions(y)
+            assert y_.loc.shape == Size([dim_outputs, batch_size])
+            assert y_.scale.shape == Size([dim_outputs, batch_size])
+
+            loc = torch.cat((y_.loc, torch.zeros(dim_delta, batch_size)))
+            loc = loc.expand(num_particles, dim_states, batch_size).permute(1, 2, 0)
+            assert loc.shape == Size([dim_states, batch_size, num_particles])
+
+            cov = torch.cat((y_.scale, torch.ones(dim_delta, batch_size)))
+            cov = cov.expand(num_particles, dim_states, batch_size).permute(1, 2, 0)
+            assert cov.shape == Size([dim_states, batch_size, num_particles])
+
+            # TODO: IMPLEMENT BACKWARDS-PASS
+            outputs.append(Normal(loc, cov))
+
+        return outputs[::-1]
+
+
+class CBFSSM(PRSSM):
+    """Conditional Backwards Forwards Algorithm."""
+
+    def _condition(self, next_x: MultivariateNormal, next_y: Normal
+                   ) -> MultivariateNormal:
+        """Condition the next_x distribution with the measurements of next_y.
+
+        Next_x is a Multivariate Normal and the covariance matrix is between particles.
+        However, between the x-coordinates is independent.
+
+        Next_y is a Normal distribution (diagonal Multivariate Normal) with the same
+        dimensionality as next_x.
+
+        In this case, the Kalman Filtering reduces to 1-D, where the gain is just:
+        k = sigma_x / (sigma_x + sigma_y).
+        The location of each particle is updated by k times the error.
+        The covariance is the prior covariance scaled by 1 - k.
+
+        Parameters
+        ----------
+        next_x: MultivariateNormal.
+            Covariance state_dim x batch_size x num_particles x num_particles.
+        next_y: Normal.
+            Scale state_dim x batch_size x num_particles.
+
+        Returns
+        -------
+        next_x: MultivariateNormal.
+            Covariance state_dim x batch_size x num_particles x num_particles.
+        """
+        error = next_x.loc - next_y.loc
+
+        sigma_f = torch.diagonal(next_x.covariance_matrix, dim1=-1, dim2=-2)
+        sigma_y = next_y.scale
+        gain = sigma_f / (sigma_f + sigma_y)
+        neg_gain = torch.diag_embed(1 - gain)
+
+        loc = next_x.loc + gain * error
+        cov = torch.diag_embed(gain.pow(2) * sigma_y)
+        cov += neg_gain @ next_x.covariance_matrix @ neg_gain
+        return MultivariateNormal(loc, cov)
