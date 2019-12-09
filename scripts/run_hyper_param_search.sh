ae92de0a132abfa4b519f34cf6855db4f7fe0b54
scuri <sebastian.curi@inf.ethz.ch>
2019-12-09 14:06:54 +0100

    Add hyper_param search experiments

---
+++ scripts/run_hyper_param_search.sh
@@ -0,0 +1,7 @@
+#python gpssm/run_experiment.py experiments/hyper_param_search/diag/config.yaml &
+#python gpssm/run_experiment.py experiments/hyper_param_search/kernel/config.yaml &
+#python gpssm/run_experiment.py experiments/hyper_param_search/loss_factors/config.yaml &
+#python gpssm/run_experiment.py experiments/hyper_param_search/num_particles/config.yaml &
+python gpssm/run_experiment.py experiments/hyper_param_search/optim/config.yaml &
+#python gpssm/run_experiment.py experiments/hyper_param_search/recognition/config.yaml &
+#python gpssm/run_experiment.py experiments/hyper_param_search/var_dist/config.yaml &
