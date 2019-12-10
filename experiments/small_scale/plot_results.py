3b68a5d41b3a29aad2d4100846306c92b450516e
scuri <sebastian.curi@inf.ethz.ch>
2019-12-10 10:09:15 +0100

    Update experiments

--- experiments/small_scale/plot_results.py
+++ experiments/small_scale/plot_results.py
@@ -4,39 +4,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import product
 
+SPLITS = ['test', 'train', 'last']
+
 
 def parse_file(file_name):
     """Parse a file."""
     with open(file_name) as file:
         data = file.readlines()
     test = data[-2].split('. ')
+    train = data[-1].split('. ')
+    last_epoch = data[-3].split('. ')
+
     test[0] = 'L' + test[0].split(' L')[1]
+    train[0] = 'L' + train[0].split(' L')[1]
+    last_epoch[0] = 'L' + last_epoch[0].split(' L')[1]
+
 
     results = {}
-    for key_val in test:
-        key, val = key_val.split(':')
-        results[key.lower()] = float(val)
+    for split, value in zip(SPLITS, [test, train, last_epoch]):
+        results[split] = {}
+        for key_val in value:
+            key, val = key_val.split(':')
+            results[split][key.lower()] = float(val)
     return results
 
-methods = ['PRSSM'] #, 'VCDT', 'CBFSSM']
+
+def process_method(method, datasets, keys):
+    print(method)
+    losses = {}
+    for dataset in datasets:
+        if dataset not in losses:
+            losses[dataset] = {}
+
+        for k in keys:
+            key = ('{}/'*len(k)).format(*k)
+            if key not in losses[dataset]:
+                losses[dataset][key] = {}
+
+            try:
+                file_name = '{}/{}/{}train_epoch.txt'.format(method, dataset, key)
+                results = parse_file(file_name)
+            except FileNotFoundError:
+                file_name = '{}/{}/{}train_epoch_0.txt'.format(method, dataset, key)
+                results = parse_file(file_name)
+
+            for split in results:
+                for loss, val in results[split].items():
+                    if loss not in losses[dataset][key]:
+                        losses[dataset][key][loss] = {}
+                    if split not in losses[dataset][key][loss]:
+                        losses[dataset][key][loss][split] = []
+
+                    losses[dataset][key][loss][split].append(val)
+
+
+    for dataset in datasets:
+        print(dataset)
+        min_loss = float('inf')
+        min_key = None
+        for key in losses[dataset]:
+            if np.mean(losses[dataset][key]['rmse']['last']) < min_loss:
+                min_key = key
+                min_loss = np.mean(losses[dataset][key]['rmse']['test'])
+        print(min_key, losses[dataset][min_key]['rmse'])
+
+
+# def process_method(method_name, datasets, var_dists, k_us, k_factor):
+
 datasets = ['Actuator', 'BallBeam', 'Drive', 'Dryer', 'GasFurnace', 'Flutter', 'Tank']
-# seeds = [0, 1, 2, 3, 4]
+var_dists = ['full', 'delta', 'full', 'mean', 'sample']
+k_us = ['0.1', '0.01', '0.05']
+k_factors = ['1', '10', '50', '100']
 
-# fig, axes = plt.subplots(len(datasets), 1, sharex=True)
-fig = plt.figure()
-losses = {}
-for method, dataset in product(methods, datasets):
-    if method not in losses:
-        losses[method] = {}
-    if dataset not in losses[method]:
-        losses[method][dataset] = {'rmse': [], 'nrmse': [], 'log-lik': []}
+process_method('CBFSSM', datasets, list(product(
+    ['delta', 'full', 'mean', 'sample'],
+    ['0.1', '0.01', '0.05'],
+    ['1', '10', '50', '100'])))
+process_method('VCDT', datasets, list(product(
+    ['sample'],
+    ['0.1', '0.01', '0.05'])))
 
-    file_name = '{}/{}/train_epoch.txt'.format(method, dataset)
-    results = parse_file(file_name)
-    for key, val in results.items():
-        losses[method][dataset][key].append(val)
-
-for key in ['rmse', 'log-lik']:
-    print(key)
-    for method, dataset in product(methods, datasets):
-        print(method, dataset, losses[method][dataset][key])
+process_method('PRSSM', datasets, [('',)])
